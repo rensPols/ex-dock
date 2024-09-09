f34e6fb36df78ed984ad5846c2f09ea3125ea8c4
Kevin Kruijthof <k.kruijthof@student.tudelft.nl>
2024-09-09 23:07:12 +0200

    Added a helper for worker and virtual Verticles

--- src/main/kotlin/com/ex_dock/ex_dock/helper/VerticleDeployHelper.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/helper/VerticleDeployHelper.kt
@@ -1,8 +1,6 @@
 package com.ex_dock.ex_dock.helper
 
-import io.vertx.core.Future
-import io.vertx.core.Promise
-import io.vertx.core.Vertx
+import io.vertx.core.*
 
 class VerticleDeployHelper {
   companion object {
@@ -29,5 +27,55 @@ class VerticleDeployHelper {
 
       return promise.future()
     }
+  }
+
+  fun deployWorkerHelper(vertx: Vertx, name: String): Future<Void> {
+    val promise: Promise<Void> = Promise.promise<Void>()
+    val options: DeploymentOptions = DeploymentOptions().setThreadingModel(ThreadingModel.WORKER)
+    vertx.deployVerticle(name, options)
+      .onComplete{ res ->
+        if (res.failed()) {
+          println(buildString {
+            append("\u001b[31m")
+            append("⨯ - Failed to worker Verticle: $name")
+            append("\u001b[0m")
+          })
+          promise.fail(res.cause())
+        } else {
+          println(buildString {
+            append("\u001b[32m")
+            append("✓ - $name worker Verticle was successfully deployed")
+            append("\u001b[0m")
+          })
+          promise.complete()
+        }
+      }
+
+    return promise.future()
+  }
+
+  fun deployVertualVerticle(vertx: Vertx, name: String): Future<Void> {
+    val promise: Promise<Void> = Promise.promise<Void>()
+    val options: DeploymentOptions = DeploymentOptions().setThreadingModel(ThreadingModel.VIRTUAL_THREAD)
+    vertx.deployVerticle(name, options)
+      .onComplete{ res ->
+        if (res.failed()) {
+          println(buildString {
+            append("\u001b[31m")
+            append("⨯ - Failed to virtual Verticle: $name")
+            append("\u001b[0m")
+          })
+          promise.fail(res.cause())
+        } else {
+          println(buildString {
+            append("\u001b[32m")
+            append("✓ - $name virtual Verticle was successfully deployed")
+            append("\u001b[0m")
+          })
+          promise.complete()
+        }
+      }
+
+    return promise.future()
   }
 }
