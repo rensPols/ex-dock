9861b8764c0f5b2e78690a2eb19eca54a47c5d30
Kevin Kruijthof <k.kruijthof@student.tudelft.nl>
2024-09-16 21:46:16 +0200

    Product get and post functions

--- src/main/kotlin/com/ex_dock/ex_dock/database/product/ProductJdbcVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/product/ProductJdbcVerticle.kt
@@ -3,11 +3,13 @@ package com.ex_dock.ex_dock.database.product
 import com.ex_dock.ex_dock.database.connection.Connection
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.eventbus.EventBus
+import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 import io.vertx.sqlclient.Pool
 import io.vertx.sqlclient.Row
 import io.vertx.sqlclient.RowSet
+import io.vertx.sqlclient.Tuple
 
 class ProductJdbcVerticle: AbstractVerticle() {
   private lateinit var client: Pool
@@ -18,13 +20,15 @@ class ProductJdbcVerticle: AbstractVerticle() {
     eventBus = vertx.eventBus()
 
     getAllProducts()
+    getProductById()
+    createProduct()
   }
 
   private fun getAllProducts() {
-    val consumer = eventBus.localConsumer<Any>("process.products.getAll")
+    val consumer = eventBus.localConsumer<JsonObject>("process.products.getAll")
     consumer.handler { message ->
       val rowsFuture = client.preparedQuery("SELECT * FROM products").execute()
-      var json: Unit;
+      var json: JsonObject;
 
       rowsFuture.onFailure { res ->
         println("Failed to execute query: $res")
@@ -49,6 +53,57 @@ class ProductJdbcVerticle: AbstractVerticle() {
         } else {
           message.reply("{}")
         }
+      }
+    }
+  }
+
+  private fun getProductById() {
+    val consumer = eventBus.localConsumer<JsonObject>("process.products.getProductById")
+    consumer.handler { message ->
+      var json: JsonObject
+      val productId = message.body().getString("productId")
+      val rowsFuture = client.preparedQuery("SELECT * FROM products WHERE product_id = ?")
+        .execute(Tuple.of(productId))
+
+      rowsFuture.onFailure{ res ->
+        println("Failed to execute query: $res")
+        message.reply("Failed!")
+      }.onSuccess{ res ->
+        val rows: RowSet<Row> = res
+        if (rows.size() > 0) {
+          val row = rows.first()
+          json = json {
+            obj(
+              "id" to row.getInteger("product_id"),
+              "name" to row.getString("name"),
+              "short_name" to row.getString("short_name"),
+              "description" to row.getString("description"),
+              "short_description" to row.getString("short_description")
+            )
+          }
+          message.reply(json)
+        } else {
+          message.reply("{}")
+        }
+      }
+    }
+  }
+
+  private fun createProduct() {
+    val consumer = eventBus.localConsumer<JsonObject>("process.products.createProduct")
+    consumer.handler { message ->
+      val product = message.body()
+      val rowsFuture = client.preparedQuery("INSERT INTO products (name, short_name, description, short_description) VALUES (?,?,?,?)")
+       .execute(Tuple.of(
+         product.getString("name").orEmpty(),
+           product.getString("short_name").orEmpty(),
+           product.getString("description").orEmpty(),
+           product.getString("short_description").orEmpty()))
+      rowsFuture.onFailure{ res ->
+        println("Failed to execute query: $res")
+        message.reply("Failed!")
+      }.onSuccess{ res ->
+        message.reply("Success!")
       }
     }
   }
--- src/main/kotlin/com/ex_dock/ex_dock/frontend/product/router/productRouter.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/product/router/productRouter.kt
@@ -3,17 +3,45 @@ package com.ex_dock.ex_dock.frontend.product.router
 import io.vertx.ext.web.Router
 import io.vertx.core.Vertx
 import io.vertx.core.eventbus.EventBus
+import io.vertx.core.json.JsonObject
+import io.vertx.ext.web.handler.BodyHandler
+import io.vertx.kotlin.core.json.Json
+import io.vertx.kotlin.core.json.obj
 
 fun Router.initProduct(vertx: Vertx) {
   val productRouter = Router.router(vertx)
   val eventBus: EventBus = vertx.eventBus()
 
+  productRouter.post("/").handler(BodyHandler.create())
+
   productRouter.get("/").handler { ctx ->
-    eventBus.request<Any>("process.products.getAll", "")
+    eventBus.request<JsonObject>("process.products.getAll", "")
       .onSuccess{ reply ->
         ctx.end(reply.body().toString())
       }.onFailure { error ->
         ctx.end("Error retrieving products: ${error.localizedMessage}")
+      }
+  }
+
+  productRouter.get("/id/:id").handler { ctx ->
+    val productId = Json.obj {
+      "productId" to ctx.pathParam("id")
+    }
+    eventBus.request<JsonObject>("process.products.getProductById", productId)
+     .onSuccess{ reply ->
+        ctx.end(reply.body().toString())
+      }.onFailure { error ->
+        ctx.end("Error retrieving product with id $productId: ${error.localizedMessage}")
+      }
+  }
+
+  productRouter.post("/").handler { ctx ->
+    val requestBody = ctx.body().asJsonObject()
+    eventBus.request<JsonObject>("process.products.createProduct", requestBody)
+     .onSuccess{ reply ->
+        ctx.end(reply.body().toString())
+      }.onFailure { error ->
+        ctx.end("Error creating product: ${error.localizedMessage}")
       }
   }
 
