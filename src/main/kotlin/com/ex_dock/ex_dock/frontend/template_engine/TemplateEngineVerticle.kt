70d42fb404f94102d5a1104d5f9969841decd04c
Rens Pols <rens@cepols.nl>
2024-10-11 18:28:29 +0200

    Initialised the TemplateEngineVerticle and created a singleUseTemplate() function #25

---
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/TemplateEngineVerticle.kt
@@ -0,0 +1,39 @@
+package com.ex_dock.ex_dock.frontend.template_engine
+
+import com.ex_dock.ex_dock.database.connection.Connection
+import com.ex_dock.ex_dock.frontend.template_engine.template_data.single_use.SingleUseTemplateData
+import com.ex_dock.ex_dock.frontend.template_engine.template_data.single_use.SingleUseTemplateDataCodec
+import io.pebbletemplates.pebble.PebbleEngine
+import io.pebbletemplates.pebble.loader.StringLoader
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.eventbus.EventBus
+import io.vertx.sqlclient.Pool
+import java.io.StringWriter
+
+class TemplateEngineVerticle: AbstractVerticle() {
+  private lateinit var client: Pool
+  private lateinit var eventBus: EventBus
+  private val engine = PebbleEngine.Builder().loader(StringLoader()).build()
+  private val failedMessage: String = "Template engine failure"
+
+  override fun start() {
+    client = Connection().getConnection(vertx)
+    eventBus = vertx.eventBus()
+
+    singleUseTemplate()
+  }
+
+  private fun singleUseTemplate() {
+    eventBus.registerCodec(SingleUseTemplateDataCodec())
+    eventBus.consumer<SingleUseTemplateData>("template.generate.singleUse") { message ->
+      val singleUseTemplateData: SingleUseTemplateData = message.body()
+
+      val compiledTemplate = engine.getTemplate(singleUseTemplateData.template)
+
+      val writer = StringWriter()
+      compiledTemplate.evaluate(writer, singleUseTemplateData.templateData)
+
+      message.reply(writer.toString())
+    }
+  }
+}
