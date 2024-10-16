ed2e5ed04a7c72a32970ffd31ae59f2e00be54d3
Rens Pols <rens@cepols.nl>
2024-10-16 21:21:47 +0200

    Convert SingleUseTemplateData class to data class

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/template_data/single_use/SingleUseTemplateData.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/template_data/single_use/SingleUseTemplateData.kt
@@ -1,11 +1,6 @@
 package com.ex_dock.ex_dock.frontend.template_engine.template_data.single_use
 
-class SingleUseTemplateData{
-  val template: String
-  val templateData: Map<String, Any?>
-
-  constructor(template: String, templateData: Map<String, Any?>) {
-    this.template = template
-    this.templateData = templateData
-  }
-}
+data class SingleUseTemplateData(
+    val template: String,
+    val templateData:Map<String, Any?>,
+)
