07cb7c077fa1f86b13883fbbb621ef1153b09c71
Rens Pols <rens@cepols.nl>
2024-10-17 14:23:58 +0200

    Optimised imports

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/FrontendVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/FrontendVerticle.kt
@@ -6,11 +6,8 @@ import com.ex_dock.ex_dock.frontend.checkout.CheckoutFrontendVerticle
 import com.ex_dock.ex_dock.frontend.home.HomeFrontendVerticle
 import com.ex_dock.ex_dock.frontend.product.ProductFrontendVerticle
 import com.ex_dock.ex_dock.frontend.text_pages.TextPagesFrontendVerticle
-import com.ex_dock.ex_dock.helper.VerticleDeployHelper
 import com.ex_dock.ex_dock.helper.deployVerticleHelper
 import io.vertx.core.AbstractVerticle
-import io.vertx.kotlin.coroutines.CoroutineVerticle
-import kotlinx.coroutines.delay
 
 class FrontendVerticle: AbstractVerticle() {
   override fun start() {
