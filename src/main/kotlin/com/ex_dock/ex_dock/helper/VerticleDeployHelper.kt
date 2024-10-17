ca606ede35c1d00181a3dc9e6c125b3d5b0259ce
Rens Pols <rens@cepols.nl>
2024-10-17 12:49:40 +0200

    Moved VerticleDeployHelper.kt functions outside of class

--- src/main/kotlin/com/ex_dock/ex_dock/helper/VerticleDeployHelper.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/helper/VerticleDeployHelper.kt
@@ -4,6 +4,8 @@ import io.vertx.core.*
 
 class VerticleDeployHelper {
   companion object {
+
+    @Deprecated("Function is moved outside of class", ReplaceWith("deployVerticleHelper()"))
     fun deployHelper(vertx: Vertx, name: String): Future<Void> {
       val promise: Promise<Void> = Promise.promise<Void>()
       vertx.deployVerticle(name)
@@ -29,6 +31,7 @@ class VerticleDeployHelper {
     }
   }
 
+  @Deprecated("Function is moved outside of class", ReplaceWith("deployWorkerVerticleHelper()"))
   fun deployWorkerHelper(vertx: Vertx, name: String, workerPoolSize: Int, instances: Int): Future<Void> {
     val promise: Promise<Void> = Promise.promise<Void>()
     val options: DeploymentOptions = DeploymentOptions()
@@ -59,6 +62,7 @@ class VerticleDeployHelper {
     return promise.future()
   }
 
+  @Deprecated("Function is moved outside of class", ReplaceWith("deployVirtualVerticleHelper()"))
   fun deployVirtualVerticle(vertx: Vertx, name: String): Future<Void> {
     val promise: Promise<Void> = Promise.promise<Void>()
     val options: DeploymentOptions = DeploymentOptions().setThreadingModel(ThreadingModel.VIRTUAL_THREAD)
@@ -83,4 +87,86 @@ class VerticleDeployHelper {
 
     return promise.future()
   }
+}
+
+
+fun deployVerticleHelper(vertx: Vertx, name: String): Future<Void> {
+  val promise: Promise<Void> = Promise.promise<Void>()
+  vertx.deployVerticle(name)
+    .onComplete{ res ->
+      if (res.failed()) {
+        println(buildString {
+          append("\u001b[31m")
+          append("⨯ - Failed to deploy Verticle: $name\nCause: ${res.cause()}")
+          append("\u001b[0m")
+        })
+        promise.fail(res.cause())
+      } else {
+        println(buildString {
+          append("\u001b[32m")
+          append("✓ - $name Verticle was successfully deployed")
+          append("\u001b[0m")
+        })
+        promise.complete()
+      }
+    }
+
+  return promise.future()
+}
+
+
+fun deployWorkerVerticleHelper(vertx: Vertx, name: String, workerPoolSize: Int, instances: Int): Future<Void> {
+  val promise: Promise<Void> = Promise.promise<Void>()
+  val options: DeploymentOptions = DeploymentOptions()
+    .setThreadingModel(ThreadingModel.WORKER)
+    .setWorkerPoolName(name)
+    .setWorkerPoolSize(workerPoolSize)
+    .setInstances(instances)
+
+  vertx.deployVerticle(name, options)
+    .onComplete{ res ->
+      if (res.failed()) {
+        println(buildString {
+          append("\u001b[31m")
+          append("⨯ - Failed to worker Verticle: $name")
+          append("\u001b[0m")
+        })
+        promise.fail(res.cause())
+      } else {
+        println(buildString {
+          append("\u001b[32m")
+          append("✓ - $name worker Verticle was successfully deployed")
+          append("\u001b[0m")
+        })
+        promise.complete()
+      }
+    }
+
+  return promise.future()
+}
+
+
+fun deployVirtualVerticleHelper(vertx: Vertx, name: String): Future<Void> {
+  val promise: Promise<Void> = Promise.promise<Void>()
+  val options: DeploymentOptions = DeploymentOptions().setThreadingModel(ThreadingModel.VIRTUAL_THREAD)
+  vertx.deployVerticle(name, options)
+    .onComplete{ res ->
+      if (res.failed()) {
+        println(buildString {
+          append("\u001b[31m")
+          append("⨯ - Failed to virtual Verticle: $name")
+          append("\u001b[0m")
+        })
+        promise.fail(res.cause())
+      } else {
+        println(buildString {
+          append("\u001b[32m")
+          append("✓ - $name virtual Verticle was successfully deployed")
+          append("\u001b[0m")
+        })
+        promise.complete()
+      }
+    }
+
+  return promise.future()
 }
