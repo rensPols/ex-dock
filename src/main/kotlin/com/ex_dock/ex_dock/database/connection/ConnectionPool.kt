e45f04582dac658174583938657711f50118eb24
Rens Pols <rens@cepols.nl>
2024-10-25 14:34:23 +0200

    Fixed getConnection()

--- src/main/kotlin/com/ex_dock/ex_dock/database/connection/ConnectionPool.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/connection/ConnectionPool.kt
@@ -6,6 +6,7 @@ import io.vertx.jdbcclient.JDBCPool
 import io.vertx.sqlclient.Pool
 import io.vertx.sqlclient.PoolOptions
 import java.util.*
+import kotlin.jvm.javaClass
 
 class Connection {
   @Deprecated("Moved getConnection out of Connection() class")
@@ -50,16 +51,33 @@ class Connection {
 
 
 fun getConnection(vertx: Vertx): Pool {
-  var connection: Pool
-
-  var props: Properties = ClassLoader.getSystemClassLoader().getResourceAsStream("secret.properties").use {
-    Properties().apply { load(it) }
-  }
-
+  val connection: Pool
   val connectOptions = JDBCConnectOptions()
-    .setJdbcUrl(props.getProperty("DATABASE_URL"))
-    .setUser(props.getProperty("DATABASE_USERNAME"))
-    .setPassword(props.getProperty("DATABASE_PASSWORD"))
+
+  try {
+    val props: Properties = ClassLoader.getSystemClassLoader().getResourceAsStream("secret.properties").use {
+      Properties().apply { load(it) }
+    }
+
+    connectOptions
+      .setJdbcUrl(props.getProperty("DATABASE_URL"))
+      .setUser(props.getProperty("DATABASE_USERNAME"))
+      .setPassword(props.getProperty("DATABASE_PASSWORD"))
+  } catch (e: Exception) {
+    try {
+      val isDocker: Boolean = !System.getenv("GITHUB_RUN_NUMBER").isNullOrEmpty()
+      if (isDocker) {
+        connectOptions
+          .setJdbcUrl("jdbc:postgresql://localhost:8890/ex-dock")
+          .setUser("postgres")
+          .setPassword("docker")
+      } else {
+        error("Could not load the Properties file!")
+      }
+    } catch (e: Exception) {
+      error("Could not read the Properties file!")
+    }
+  }
 
   val poolOptions = PoolOptions()
     .setMaxSize(16)
