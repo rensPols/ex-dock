69a16e371e3decc5b663ff649440bfc21475ec44
Rens Pols <rens@cepols.nl>
2024-11-15 15:40:58 +0100

    Initialised the HomeFrontendVerticle functionality #37

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/home/HomeFrontendVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/home/HomeFrontendVerticle.kt
@@ -1,6 +1,49 @@
 package com.ex_dock.ex_dock.frontend.home
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.eventbus.EventBus
 
 class HomeFrontendVerticle: AbstractVerticle() {
+
+  private lateinit var eventBus: EventBus
+
+  override fun start() {
+    eventBus = vertx.eventBus()
+
+    getHomePage()
+  }
+
+  private fun getHomePage() {
+    val getHomeConsumer = eventBus.consumer<Any?>("frontend.retrieveHTML.home")
+    getHomeConsumer.handler { message ->
+      val data = getHomeData()
+      val template = retrieveHomeTemplate()
+      eventBus.request<String>("template.generate.compiled", "home").onFailure {
+        // TODO: implement
+      }.onSuccess { response ->
+        message.reply(response)
+      }
+    }
+  }
+
+  private fun retrieveHomeTemplate(): String {
+    // TODO: get template from templates table with key "home"
+    return """
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>exDock Home Page</title>
+        </head>
+        <body>
+          <h1>Welcome to exDock!</h1>
+          <p>This is the home page of the exDock server.</p>
+        </body>
+      </html>
+    """.trimIndent();
+  }
+
+  private fun getHomeData(): Map<String, Any> {
+    // TODO: get data needed for the home page
+    return mapOf()
+  }
 }
