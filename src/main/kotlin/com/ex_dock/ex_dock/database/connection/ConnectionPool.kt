537cbed548dc6b066d252131b5a00716cd466112
Kevin Kruijthof <kev17062004@gmail.com>
2024-11-15 13:51:24 +0100

    Added a dummy classloader

--- src/main/kotlin/com/ex_dock/ex_dock/database/connection/ConnectionPool.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/database/connection/ConnectionPool.kt
@@ -1,5 +1,6 @@
 package com.ex_dock.ex_dock.database.connection
 
+import com.ex_dock.ex_dock.ClassLoaderDummy
 import com.ex_dock.ex_dock.MainVerticle
 import io.vertx.core.Vertx
 import io.vertx.jdbcclient.JDBCConnectOptions
@@ -55,7 +56,7 @@ fun getConnection(vertx: Vertx): Pool {
   val connectOptions = JDBCConnectOptions()
 
   try {
-    val props: Properties = MainVerticle::class.java.classLoader.getResourceAsStream("secret.properties").use {
+    val props: Properties = ClassLoaderDummy::class.java.classLoader.getResourceAsStream("secret.properties").use {
       Properties().apply { load(it) }
     }
 
