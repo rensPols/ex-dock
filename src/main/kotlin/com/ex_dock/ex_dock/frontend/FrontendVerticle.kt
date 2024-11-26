25d0b34cf9dd83361d308bd7ce2682779ba1c59b
Rens Pols <rens@cepols.nl>
2024-11-26 22:16:33 +0100

    Start the TemplateEngineVerticle in the FrontendVerticle

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/FrontendVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/FrontendVerticle.kt
@@ -5,6 +5,7 @@ import com.ex_dock.ex_dock.frontend.category.CategoryFrontendVerticle
 import com.ex_dock.ex_dock.frontend.checkout.CheckoutFrontendVerticle
 import com.ex_dock.ex_dock.frontend.home.HomeFrontendVerticle
 import com.ex_dock.ex_dock.frontend.product.ProductFrontendVerticle
+import com.ex_dock.ex_dock.frontend.template_engine.TemplateEngineVerticle
 import com.ex_dock.ex_dock.frontend.text_pages.TextPagesFrontendVerticle
 import com.ex_dock.ex_dock.helper.deployVerticleHelper
 import io.vertx.core.AbstractVerticle
@@ -17,5 +18,7 @@ class FrontendVerticle: AbstractVerticle() {
     deployVerticleHelper(vertx, CheckoutFrontendVerticle::class.qualifiedName.toString())
     deployVerticleHelper(vertx, AccountFrontendVerticle::class.qualifiedName.toString())
     deployVerticleHelper(vertx, TextPagesFrontendVerticle::class.qualifiedName.toString())
+
+    deployVerticleHelper(vertx, TemplateEngineVerticle::class.qualifiedName.toString())
   }
 }
