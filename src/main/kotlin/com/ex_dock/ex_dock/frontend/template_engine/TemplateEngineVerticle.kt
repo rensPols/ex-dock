190457815e8a5804da1d89b33c7a70a5d81f77ca
Rens Pols <rens@cepols.nl>
2024-11-27 15:54:20 +0100

    removed log statements

--- src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/TemplateEngineVerticle.kt
+++ src/main/kotlin/com/ex_dock/ex_dock/frontend/template_engine/TemplateEngineVerticle.kt
@@ -21,16 +21,11 @@ class TemplateEngineVerticle: AbstractVerticle() {
   private var compiledTemplates: MutableMap<String, PebbleTemplate> = mutableMapOf()
 
   override fun start() {
-    println("[CHECK] TemplateEngineVerticle | check 0")
     client = getConnection(vertx)
-    println("[CHECK] TemplateEngineVerticle | check 1")
     eventBus = vertx.eventBus()
-    println("[CHECK] TemplateEngineVerticle | check 2")
 
     singleUseTemplate()
-    println("[CHECK] TemplateEngineVerticle | check 3")
     compiledTemplate()
-    println("[CHECK] TemplateEngineVerticle | check 4")
   }
 
   private fun singleUseTemplate() {
@@ -49,32 +44,24 @@ class TemplateEngineVerticle: AbstractVerticle() {
 
   // TODO: test
   private fun compiledTemplate() {
-    println("[CHECK] compiledTemplate() | check 0")
     compileAllTemplates()
-    println("[CHECK] compiledTemplate() | check 1")
 
     // TODO: make a global data variable for this verticle
     // TEMP:
     var data: Map<String, Any?> = mapOf(
       "name" to "testName",
-      "secondName" to "testSecondName"
+      "secondName" to "testSecondName",
+      "test" to "this is the test variable"
     )
 
-    println("[CHECK] compiledTemplate() | check 2")
     eventBus.localConsumer<String>("template.generate.compiled") { message ->
-      println("[CHECK] eventBus: template.generate.compiled | check 0")
       var compiledTemplate: PebbleTemplate? = compiledTemplates[message.body()]
 
-      println("[CHECK] eventBus: template.generate.compiled | check 1")
-
       if (compiledTemplate != null) {
-        println("[CHECK] eventBus: template.generate.compiled -- is compiled | check 2.0")
         val writer = StringWriter()
-        println("[CHECK] eventBus: template.generate.compiled -- is compiled | check 2.1")
         compiledTemplate.evaluate(writer, data)
-        println("[CHECK] eventBus: template.generate.compiled -- is compiled | check 2.2")
         message.reply(writer.toString())
-        println("[CHECK] eventBus: template.generate.compiled -- is compiled | check 2.3")
+        // TODO: sometimes refetch template
         return@localConsumer
       }
 
@@ -84,36 +71,25 @@ class TemplateEngineVerticle: AbstractVerticle() {
         println("Failed to execute query: $e")
         message.reply("A failure occurred during the generation of the template, attempted template: ${message.body()}")
       }.onSuccess { rowSet: RowSet<Row> ->
-        println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.0")
         if (rowSet.rowCount() > 0) {
-          println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.1")
           compiledTemplate = engine.getTemplate(rowSet.first().getString("template_data"))
-          println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.2")
           compiledTemplates.put(
             rowSet.first().getString("template_key"),
             compiledTemplate
           )
-          println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.3")
 
           val writer = StringWriter()
-          println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.4")
           compiledTemplate.evaluate(writer, data)
-          println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.5")
           message.reply(writer.toString())
-          println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 3.6")
           return@onSuccess
         }
-        println("[CHECK] eventBus: template.generate.compiled -- is not compiled | check 4.0")
         message.reply("The '${message.body()}' template does not exist in the database")
       }
     }
-    println("[CHECK] compiledTemplate() | check 3")
   }
 
   private fun compileAllTemplates() {
-    println("[CHECK] compiledAllTemplate() | check 0")
     var templates: MutableMap<String, String> = mutableMapOf()
-    println("[CHECK] compiledAllTemplate() | check 1")
     client.preparedQuery(
       "SELECT template_key, template_data FROM templates"
     ).execute().onFailure { err ->
@@ -121,20 +97,15 @@ class TemplateEngineVerticle: AbstractVerticle() {
       println("err.message: ${err.message}")
     }.onSuccess { res ->
       res.forEach { templateRow ->
-        println("[CHECK] compiledAllTemplate() | check 1.1")
         templates.put(
           templateRow.getString("template_key"),
           templateRow.getString("template_data")
         )
       }
 
-      println("[CHECK] compiledAllTemplate() | check 2")
       for (template in templates) {
-        println("[CHECK] compiledAllTemplate() | check 2.1")
         compiledTemplates.put(template.key, engine.getTemplate(template.value))
-        println("[CHECK] compiledAllTemplate() | check 2.2")
       }
     }
-    println("[CHECK] compiledAllTemplate() | check 3")
   }
 }
